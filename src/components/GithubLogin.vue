0422b744d46159a5dd9f06a9faf62ca69a53a022
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-05-20 14:48:41 +0100

    Process callback from api

--- src/components/GithubLogin.vue
+++ src/components/GithubLogin.vue
@@ -1,35 +1,24 @@
 <template>
   <div class="github-login">
-    <p class="github-login-description">To start using this service authenticate using Github</p>
-    <button class="github-login-btn" v-on:click="startSequence">Login with Github</button>
+    <p v-if="!isAuthenticated" class="github-login-description">To start using this service authenticate using Github</p>
+    <button v-if="!isAuthenticated" class="github-login-btn" v-on:click="startSequence">Login with Github</button>
+    <button v-if="isAuthenticated" class="github-logout-btn" v-on:click="logout">Logout</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GithubLogin',
+  computed: {
+    isAuthenticated: () => window.sessionStorage.getItem('github_access_token') != null
+  },
   methods: {
     startSequence: function (event) {
-      let state
-      if (window.crypto != null) {
-        state = crypto.getRandomValues(new Uint32Array(1))[0]
-      } else {
-        state = Math.random()
-      }
-
-      window.sessionStorage.setItem('state', state)
-
-      let github = {
-        baseUrl: 'https://github.com',
-        authorizePath: '/login/oauth/authorize',
-        clientId: '207c9ed5142c1d5b74a0',
-        scope: 'user:email,read:org',
-        allowSignup: 'false',
-        state: state
-      }
-      let authorizationUrl = `${github.baseUrl}${github.authorizePath}?client_id=${github.clientId}&scope=${github.scope}&allow_signup=${github.allowSignup}&state=${github.state}`
-      console.log('going to url', authorizationUrl)
-      window.location.href = authorizationUrl
+      window.location.href = 'https://api.commits.dasmicrobot.com/oauth/github/login'
+    },
+    logout: function (event) {
+      // TODO does not trigger change in isAuthenticated
+      window.sessionStorage.removeItem('github_access_token')
     }
   }
 }
--- src/githubCallback.js
+++ src/githubCallback.js
@@ -3,31 +3,18 @@ const urlParse = require('url-parse')
 const parsedUrl = urlParse(window.location.href, true)
 const stateMessageNode = document.getElementById('state')
 
-function checkState (candidateState, cb) {
-  console.log('checking state')
-  const state = window.sessionStorage.getItem('state')
-  if (!state || !state.trim().length) {
-    return cb(new Error('Something is wrong, you either did not initiate the request or browser storage is disabled'))
-  }
+stateMessageNode.textContent = 'Extracting token'
 
-  if (!candidateState || !candidateState.trim().length || candidateState !== state) {
-    return cb(new Error('Request was tampered with!'))
-  }
+const githubAccessToken = parsedUrl.query.access_token
 
-  cb()
+if (githubAccessToken == null || !githubAccessToken.trim().length) {
+  stateMessageNode.textContent = 'Failed, redirecting ...'
+  window.sessionStorage.removeItem('github_access_token')
+} else {
+  stateMessageNode.textContent = 'Done, redirecting ...'
+  window.sessionStorage.setItem('github_access_token', githubAccessToken)
 }
 
-function exchangeCodeForToken (code) {
-  console.log('exchanging code for token')
-}
-
-checkState(parsedUrl.query.state, (err) => {
-  if (err) {
-    stateMessageNode.textContent = err.message
-    return
-  }
-
-  stateMessageNode.textContent = 'States match'
-
-  exchangeCodeForToken(parsedUrl.query.code)
-})
+setTimeout(function () {
+  window.location.replace('/')
+}, 1000)
