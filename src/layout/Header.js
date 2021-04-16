048a7af1d446df7b6b055b7a106a243f81ace787
ivarprudnikov <ivar.prudnikov@gmail.com>
2021-04-16 22:44:22 +0100

    refactor header closes #4

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,13 +1,25 @@
 import Link from 'next/link'
+import { useAuthContext } from '../auth/AuthContextProvider'
 
 function Header () {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuthContext()
   return (
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <Link href="/">
-        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3">
-          Git Activity
-        </a>
-      </Link>
+    <header>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container">
+          <Link href="/">
+            <a className="navbar-brand">
+              <img src="/logo.svg" alt="Git Activity" width="60" height="30"/>
+              <span className="small ml-2">Git Activity</span>
+            </a>
+          </Link>
+          <ul className="navbar-nav ml-auto">
+            { isAuthenticated && <li className="nav-item"><Link href="/github/organisations"><a className="nav-link">Your organisations</a></Link></li> }
+            { isAuthenticated && <li className="nav-item"><button className="ml-4 btn btn-primary" onClick={logout}>Logout</button></li> }
+            { !isAuthenticated && <li className="nav-item"><button className="btn btn-primary" onClick={loginWithRedirect}>Login with Github</button></li> }
+          </ul>
+        </div>
+      </nav>
     </header>
   )
 }
