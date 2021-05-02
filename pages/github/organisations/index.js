25e8b7d440c68f2c2570c5d2f6dc3f4b50ef64cf
ivarprudnikov <ivar.prudnikov@gmail.com>
2021-05-02 12:54:49 +0100

    cosmetic: use Section component when rendering organisations

--- pages/github/organisations/index.js
+++ pages/github/organisations/index.js
@@ -1,21 +1,19 @@
 import { useAuthContext } from '../../../src/auth/AuthContextProvider'
 import { Organisations } from '../../../src/github/Organisations'
 import Page from '../../../src/layout/Page'
+import Section from '../../../src/layout/Section'
+import React from 'react'
 
 function OrganisationsPage () {
 
   const { isAuthenticated } = useAuthContext()
 
   return (<Page>
-    <div className="container">
-      <div className="row align-items-center h-100">
-        <main className="col-md-6 offset-md-3">
-          <h1 className="h3">Your organisations</h1>
-          <p>Select an available organisation to begin filtering your activity</p>
-          {isAuthenticated && <Organisations/>}
-        </main>
-      </div>
-    </div>
+    <Section>
+      <h1 className="h3">Your organisations</h1>
+      <p>Select an available organisation to begin filtering your activity</p>
+      {isAuthenticated && <Organisations/>}
+    </Section>
   </Page>)
 }
 
